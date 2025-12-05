8ae375e260bb1cee9e44f9f378b950fed2e1291a
ysheep0906 <wlq94814@naver.com>
2025-12-05 15:57:07 +0900

    Fix repo name

--- enter_crawler.py
+++ enter_crawler.py
@@ -10,7 +10,7 @@ useragent_list = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36 Edg/131.0.2903.86"
-]
+] 
 
 header = {
   "User-Agent": random.choice(useragent_list),
